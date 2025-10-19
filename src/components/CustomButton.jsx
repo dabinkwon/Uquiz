8ba0eeb09b88c72886b314bd2364a7f678239ad8
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-10-19 14:15:11 +0900

    ✨ Feat: twMerge 패키지 적용으로 Tailwind 클래스 관리 개선
    CustomButton 컴포넌트에 twMerge 적용하여 클래스 병합 로직 단순화

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,12 @@
+import { twMerge } from "tailwind-merge";
+
 const CustomButton = ({ onClick, className = "", children }) => {
+  const classNameMerge = twMerge(
+    "py-1.5 px-5 text-white rounded-sm hover:bg-gray-500 cursor-pointer",
+    className
+  );
   return (
-    <button
-      onClick={onClick}
-      className={`${className} py-1.5 px-5 text-white rounded-sm hover:bg-gray-500 cursor-pointer`}
-    >
+    <button onClick={onClick} className={classNameMerge}>
       {children}
     </button>
   );
