ff6914b39e5b3065d77f1d4b164ca85a767bfb0f
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-10-19 15:22:09 +0900

    ✨ Feat: localstorage에 데이터 관리
    새로고침 이후에도 데이터가 유지되도록 localstorage를 활용해 상태 관리 로직 구현

--- src/context/RankingContext.jsx
+++ src/context/RankingContext.jsx
@@ -1,9 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const RankingContext = createContext();
 
 const RankingProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("ranking_info");
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+    const parsedData = JSON.parse(storedData);
+    setResult(parsedData);
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("ranking_info", JSON.stringify(result));
+    }
+  }, [result]);
 
   const addRanking = (nickname, score) => {
     setResult((prev) => {
@@ -14,6 +32,11 @@ const RankingProvider = ({ children }) => {
       return newList;
     });
   };
+
+  if (isLoading) {
+    return <div className="flex justify-center mt-2.5 text-3xl">Loading</div>;
+  }
+
   return (
     <RankingContext value={{ result, addRanking }}>{children}</RankingContext>
   );
